046bc53cf21293318a21fcf34288dc2a0ce53c70
werdffelynir <werdffelynir@gmail.com>
2026-02-06 22:08:10 +0200

    update 0.006

---
+++ src/IManager.js
@@ -0,0 +1,39 @@
+import {ICommander} from "./ICommander.js";
+
+export class IManager extends ICommander {
+
+    constructor(register) {
+        super(register)
+
+        this.archive = new Map();
+    }
+
+    get(key) {
+        return this.archive.get(key)
+    }
+
+    has(key) {
+        return this.archive.has(key)
+    }
+
+    each(cb) {
+        this.archive.forEach(cb)
+    }
+
+    set(key, value) {
+        if (this.archive.has(key)) {
+            console.warn(`Identical Keys Error. addStack parameter [${key}] is exist!`);
+            return;
+        }
+
+        return this.archive.set(key, value);
+    }
+
+    /**
+     * @param asCopyObject
+     * @returns {any|Map<any, any>}
+     */
+    getArchive(asCopyObject = false) {
+        return asCopyObject ? Object.fromEntries(this.archive.entries()) : this.archive;
+    }
+}
